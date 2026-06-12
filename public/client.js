c7c92e36058eb6c81e6368178dfd8d2ca79487bf
mopsiuserbotik <tochibacha@gmail.com>
2026-06-12 21:48:04 +0300

    4567890
    098767890

--- public/client.js
+++ public/client.js
@@ -1095,6 +1095,7 @@ function handleServerMessage(message) {
 
   if (message.type === "sfx") {
     if (message.name === "shot") addShotEffect(message);
+    if (message.name === "shahed") return;
     playSfx(message.name, message);
   }
 
@@ -1283,6 +1284,7 @@ function addFlightEffect(detail) {
     const removed = flightEffects.shift();
     stopFlightSfx(removed?.id);
   }
+  if (effect.kind === "shahed") startFlightSfx(effect);
   refreshMapEffects(duration);
 }
 
